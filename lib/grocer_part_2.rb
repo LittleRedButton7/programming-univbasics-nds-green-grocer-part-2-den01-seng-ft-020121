1352622fc50b792f8eadfd83a88afd3831fc69e3
Marissa Nolan <ritz1484@cs.com>
2021-01-28 20:16:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -39,6 +39,6 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart)
-  clearanced_cart = clearanced_cart(couponed_cart)
+  final_cart = clearanced_cart(couponed_cart)
   
 end
